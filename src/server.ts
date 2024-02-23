35168055316a782a699f83fe953f0b2919747f64
GABrambila <brambila.gabriel26@gmail.com>
2024-02-22 22:41:44 -0300

    aula22-02

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastify } from "fastify";
+import {z} from 'zod'
 
 const app = fastify()
 
@@ -6,8 +7,23 @@ app.get('/unipar', ()=> {
     return 'Olá Fastify!'
 } ) 
 
+app.post('/uniparpost', (request)=>{
+
+    const requestBody = z. object(
+        {
+            nome : z.string(), 
+            sobrenome : z.string(), 
+            idade : z.number(), 
+        }
+    )
+    const pessoa = requestBody.parse(request.body)
+
+    return pessoa
+
+    
+})
+
 app.listen({ port: 3333 }).then( ()=> {
     console.log('SERVIDOR RODANDO')
 })
-
 
