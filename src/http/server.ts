59b9afa0f60f21ba9570df89606c837acb14fa0c
GABrambila <brambila.gabriel26@gmail.com>
2024-03-18 20:40:59 -0300

    obterUSRvotos

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,6 +6,7 @@ import { prisma } from "../lib/prisma";
 import { criarEnquete } from "./routes/criar-enquete";
 import { listarEnquetes } from "./routes/listar-enquetes";
 import { obterEnquete } from "./routes/obter-enquete";
+import { votarEnquete } from "./routes/votar-enquete";
 
 const app = fastify()
 app.register(criarEnquete)
@@ -15,7 +16,9 @@ app.register(criarEnquete)
 })*/
 app.register(listarEnquetes)
 app.register(obterEnquete)
+app.register(votarEnquete)
 
 app.listen({port: 3333}).then( () => {
     console.log('SERVIDOR RODANDO')
 })
+
